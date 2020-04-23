dd5dbc32d304a90edde9ad8a49e7881d2801f979
samg9 <samgrewal.5@hotmail.com>
2020-04-23 00:21:40 -0700

    rm starter code

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import FileDropper from './components/FileDropper/FileDropper.js';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  selectedFile: null,
+  loaded: 0,
+  isLoading: false,
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  onChangeHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+    })
+  }
+  onClickHandler = () => {
+    const data = new FormData()
+    data.append('file', this.state.selectedFile)
+    axios.post("http://localhost:5000/api/upload", data, { // receive two parameter endpoint url ,form data 
+    }).then(res => { // then print response status
+      this.setState({
+        isLoading: false,
+      });
+      console.log(res.statusText)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <FileDropper onChangeHandler={this.onChangeHandler} onClickHandler={this.onClickHandler} />
+      </div>
+    );
+  }
 }
 
 export default App;
