d67008dc25462581fdbddfa2be818617d6b2cde8
samg9 <samgrewal.5@hotmail.com>
2020-04-24 04:32:38 -0700

    handle response for base64 image data, show spinner, show img

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import FileDropper from './components/FileDropper/FileDropper.js';
 import axios from 'axios';
+import { MetroSpinner } from "react-spinners-kit";
 
 const initialState = {
   selectedFile: null,
   loaded: 0,
-  isLoading: false,
+  loading: false,
+  img: null,
 }
 
 class App extends Component {
@@ -22,14 +24,20 @@ class App extends Component {
     })
   }
   onClickHandler = () => {
+    this.setState({
+      loading: true,
+    });
     const data = new FormData()
     data.append('file', this.state.selectedFile)
     axios.post("http://localhost:5000/api/upload", data, { // receive two parameter endpoint url ,form data 
     }).then(res => { // then print response status
       this.setState({
-        isLoading: false,
+        loading: false,
+        img: res.data
       });
-      console.log(res.statusText)
+      console.log(this.state.img);
+      console.log(res);
+      console.log(res.statusText);
     })
   }
 
@@ -37,6 +45,14 @@ class App extends Component {
     return (
       <div className="App">
         <FileDropper onChangeHandler={this.onChangeHandler} onClickHandler={this.onClickHandler} />
+        <div id="inner">
+          <MetroSpinner
+            size={150}
+            loading={this.state.loading}
+          />
+          <img src={this.state.img} alt="" />
+        </div>
+
       </div>
     );
   }
