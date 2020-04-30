fe15c49e01699164c4b7e424445286e255d9d88b
samg9 <samgrewal.5@hotmail.com>
2020-04-30 16:49:08 -0700

    Revert "clean up fe"
    This reverts commit 26ceeb42890d67929c6ee597f7e5bb2a168763cb.

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -58,13 +58,14 @@ class App extends Component {
           />
         </div>
         <div className="column">
-          {this.state.imageBefore == null ? "" : <img width="90%" src={this.state.imageBefore} alt="before" />}
+          {this.state.imageBefore == null ? "" : <img width="100%" height="calc(100% - 10px)" src={this.state.imageBefore} alt="before" />}
         </div>
         {this.state.imgAfter == null ? "" :
-          <div className="column">
+          <div>
+            <br />
             <a href={this.state.imgAfter} download>
-              Click on image to download
-              <img width="90%" src={this.state.imgAfter} alt="after" />
+              <h5>Click on image to download</h5>
+              <img width="100%" height="calc(100% - 10px)" src={this.state.imgAfter} alt="after" />
             </a>
           </div>
         }
--- front_end/src/components/FileDropper/FileDropper.js
+++ front_end/src/components/FileDropper/FileDropper.js
@@ -7,7 +7,7 @@ const FileDropper = ({ onChangeHandler, onClickHandler }) => {
             <div className="row">
                 <div className="offset-md-3 col-md-6">
                     <div className="form-group files">
-                        <label><b>Upload Your File to be Upscaled</b></label>
+                        <label>Upload Your File to be Upscaled</label>
                         <input type="file" className="form-control" multiple onChange={onChangeHandler} />
                     </div>
                     <div className="form-group">
@@ -15,7 +15,7 @@ const FileDropper = ({ onChangeHandler, onClickHandler }) => {
 
                     </div>
 
-                    <button type="button" className="btn btn-success btn-block" onClick={onClickHandler}>Upscale</button>
+                    <button type="button" className="btn btn-success btn-block" onClick={onClickHandler}>Upload</button>
 
                 </div>
             </div>
